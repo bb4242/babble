fe7587fa4c1be9515b8a2e279c19d0b34e738b63
Brett Bethke <10068296+bb4242@users.noreply.github.com>
2018-05-30 22:51:35 -0700

    Add external API documentation

--- lib/babble.ex
+++ lib/babble.ex
@@ -1,18 +1,82 @@
 defmodule Babble do
   @moduledoc """
-  Documentation for Babble.
+  A system for publishing messages between nodes.
+
+  - API examples
+  - Talk about TCP and UDP multicast transport
+  - ETS storage
   """
+
+  @typedoc """
+  Topics are represented as strings or atoms.
+
+  Topics may be either local (of the form `"my.topic"`) or global (of the form `{:"node@host", "my.topic"}`)
+  """
+  @type topic :: String.t() | atom() | {node(), String.t()} | {node(), atom()}
 
   @doc """
-  Hello world.
+  Publish a message to the specified topic.
 
-  ## Examples
+  `topic` is a String or atom specifying the name of the topic to publish to.
 
-      iex> Babble.hello
-      :world
+  `message` is a map of key/value pairs to publish.
 
   """
-  def hello do
-    :world
+  @spec publish(topic :: topic, message :: map()) :: :ok | {:error, reason :: String.t()}
+  def publish(topic, message) do
+    :ok
+  end
+
+  @doc """
+  Subscribe to a topic.
+
+  ## Options
+
+    * `:rate :: float() | :on_publish` - The rate to subscribe to the topic at.
+
+      * If `:rate` is a float, it is interpreted as a rate in Hz. If the topic is remote,
+      it will be transmitted over the network via UDP multicast. This is appropriate
+      for topics that are published at regular intervals, such as periodic sensor readings.
+
+      * If `:rate` is `:on_publish`, the subscriber will receive every published message.
+      If the topic is remote, it will be transmitted over the network via
+      TCP, which is a reliable transport. This is appropriate for topics that are
+      published only intermittently, such as configuration data.
+
+      * The default is `:on_publish`.
+
+    * `:deliver :: bool()` - Whether to deliver messages to the subscriber process.
+
+      * If `:deliver` is `true`, the subscribing process will receive messages of the form
+      `{:babble_msg, topic :: topic, message :: map(), timestamp :: float()}` when new topic data arrives.
+
+      * If the topic is remote, a subscription is always required to cause it to be transmitted
+      over the network. However, the subscribing process may wish to access the topic data
+      using the polling interface rather than having messages delivered to its mailbox.
+      In this case, it should call `subscribe` with `:deliver` set to `false`.
+
+      * Calling `subscribe` on a local topic with `:deliver` set to `false` has no effect.
+
+      * The default is `true`.
+
+  """
+  @spec subscribe(topic :: topic, options :: keyword()) :: :ok
+  def subscribe(topic, options \\ []) do
+    :ok
+  end
+
+  @doc """
+  Retrieve the last received values for keys published to the specified topic
+  """
+  @spec poll(topic :: topic, keys :: list() | :all, stale_time :: float()) ::
+          {:ok, list()} | {:error, reason :: String.t()}
+  def poll(topic, keys \\ [], stale_time \\ :none) do
+    {:ok, []}
+  end
+
+  @doc """
+  Sets the timesource for topic publication timestamps
+  """
+  def set_time_source(time_fun) do
   end
 end
--- mix.exs
+++ mix.exs
@@ -22,8 +22,9 @@ defmodule Babble.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false},
+      {:credo, "~> 0.9.2", only: :dev, runtime: false}
     ]
   end
 end
