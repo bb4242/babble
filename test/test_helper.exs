4fc93fad0a8c9c622fbe72889f8034db9b2c2177
Brett Bethke <10068296+bb4242@users.noreply.github.com>
2018-06-03 18:37:11 -0700

    Make sure epmd is started for cluster tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,10 @@ File.mkdir_p(Path.dirname(JUnitFormatter.get_report_file_path()))
 ExUnit.configure(formatters: [JUnitFormatter, ExUnit.CLIFormatter])
 
 Application.stop(:babble)
+Port.open({:spawn, "epmd -daemon"}, [])
 Node.start(:"test-primary@127.0.0.1", :longnames)
+# TODO: Set random cookie for primary and slave nodes
+#Node.set_cookie(Node.self(), :my_cookie)
 Application.start(:babble)
+
 ExUnit.start()
