c1216f211748c1c2a842bd70861564cb1546ae04
Brett Bethke <10068296+bb4242@users.noreply.github.com>
2018-06-07 22:26:34 -0700

    Add rate decimation support for local subscribers

--- lib/babble/pub_worker.ex
+++ lib/babble/pub_worker.ex
@@ -9,8 +9,8 @@ defmodule Babble.PubWorker do
 
   defmodule State do
     @moduledoc "State for PubWorker"
-    @enforce_keys [:topic, :table]
-    defstruct [{:next_pub_times, %{}}, :topic, :table]
+    @enforce_keys [:pub_time_table, :topic, :table]
+    defstruct [:pub_time_table, :topic, :table]
   end
 
   # Client API
@@ -54,9 +54,14 @@ defmodule Babble.PubWorker do
   # Server callbacks
   @impl true
   def init(topic) do
+    # Instantiate the main topic table
     topic = {node, _} = fully_qualified_topic_name(topic)
     table = table_name(topic)
     {:ok, ^table} = Babble.TableHeir.get_table(table)
+
+    # Instantiate the companion publish time table
+    pub_time_table_name = String.to_atom("pub_times:" <> Atom.to_string(table))
+    pub_time_table = :ets.new(pub_time_table_name, [:named_table, :protected, :set])
 
     # If this is a remote topic, monitor the remote node so we can
     # delete the table when the node goes down
@@ -64,7 +69,7 @@ defmodule Babble.PubWorker do
       Node.monitor(node, true)
     end
 
-    {:ok, %State{topic: topic, table: table}}
+    {:ok, %State{topic: topic, table: table, pub_time_table: pub_time_table}}
   end
 
   @impl true
@@ -76,7 +81,7 @@ defmodule Babble.PubWorker do
   @impl true
   def handle_cast(
         {:publish, message, options},
-        state = %State{topic: topic, table: table, next_pub_times: _next_pub_times}
+        state = %State{topic: topic, table: table, pub_time_table: pub_time_table}
       ) do
     # TODO: Insert timestamp into update
 
@@ -91,7 +96,7 @@ defmodule Babble.PubWorker do
     :ets.insert(table, msg_kw)
 
     # Publish to all local subscribers
-    send_to_local_subscribers(topic, {:babble_msg, topic, msg_map})
+    send_to_local_subscribers(topic, {:babble_msg, topic, msg_map}, pub_time_table)
 
     # Publish to remote subscribers
     for pub_node <- get_publication_nodes(topic, options) do
@@ -114,23 +119,46 @@ defmodule Babble.PubWorker do
 
   # Helpers
 
-  defp send_to_local_subscribers({pub_node, short_name}, msg) do
-    send_to_local_subscribers(pub_node, short_name, msg)
-    send_to_local_subscribers(:*, short_name, msg)
+  defp send_to_local_subscribers({pub_node, short_name}, msg, pub_time_table \\ nil) do
+    send_to_local_subscribers(pub_node, short_name, msg, pub_time_table)
+    send_to_local_subscribers(:*, short_name, msg, pub_time_table)
   end
 
-  defp send_to_local_subscribers(node, short_name, msg) do
+  defp send_to_local_subscribers(node, short_name, msg, pub_time_table) do
+    now = System.monotonic_time(:milliseconds)
+
     case Babble.poll(@subscription_topic, [{node, short_name}]) do
       {:ok, [subs]} ->
         # Get the pid and subscription rate for all subscribers who want delivery
-        for {pid, sub} <- subs, sub[:deliver] do
-          # TODO: Handle decimated delivery specified by sub[:rate]
-          # TODO: include timestamp
-          Process.send(pid, msg, [])
+        for {pid, sub} <- subs, sub[:deliver] == true do
+          # TODO: include timestamp in message
+          rate = sub[:rate]
+
+          next_pub_time =
+            if is_nil(pub_time_table) do
+              now
+            else
+              case :ets.lookup(pub_time_table, pid) do
+                [{^pid, pt}] -> pt
+                [] -> now
+              end
+            end
+
+          cond do
+            rate == :on_publish ->
+              Process.send(pid, msg, [])
+
+            is_number(rate) and now >= next_pub_time ->
+              Process.send(pid, msg, [])
+              :ets.insert(pub_time_table, [{pid, now + 1000 / rate}])
+
+            true ->
+              nil
+          end
         end
 
       {:error, _} ->
-        []
+        nil
     end
   end
 
